d0935a8c9f9238953b3a058889ce6ce04943777e
shruti bodhale <bodhales03@gmail.com>
2026-08-14 00:42:35 +0530

    Add files via upload

---
+++ number_gussing.py
@@ -0,0 +1,25 @@
+# Number Guessing Game
+
+import random
+
+# Generate a random number between 1 and 100
+number = random.randint(1, 100)
+
+print("Guess the number between 1 and 100")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+
+        if guess < number:
+            print("Too low! Try again.")
+
+        elif guess > number:
+            print("Too high! Try again.")
+
+        else:
+            print("Congratulations! You guessed the correct number.")
+            break
+
+    except ValueError:
+        print("Please enter a valid number.")
